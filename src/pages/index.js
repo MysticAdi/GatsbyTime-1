39910dadaa4902a69ed1d9cd2f86b1e558c1ccc1
MysticAdi <adityapt97@gmail.com>
2019-12-02 15:56:42 +0530

    First few changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
-import React from "react"
+import React,{Component} from "react";
+import {Link} from "gatsby";
+import {Banner,TextWrapper, MoreText} from "../styles/IndexStyles";
+import { relative } from "path";
 
-export default () => <div>Hello world!</div>
+
+export default () => (
+    <div style ={{position:relative}}>
+        <Banner></Banner>
+        <TextWrapper>
+            <div>
+                <h2> Mr.0bvious </h2>
+                <p> Just a simple <br/>
+                Competitive Coder & <br/>
+                Web Developer</p>
+                <Link to="/works">My Works</Link>
+            </div>
+        </TextWrapper>
+        <MoreText>Learn More</MoreText>
+    </div>
+)
